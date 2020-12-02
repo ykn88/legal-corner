40fc248580e392a628ca6029b897cfd9cf5c60bb
saqlain05 <roseisred9821@gmail.com>
2020-12-02 14:53:03 +0530

    news

--- components/backend/Header.js
+++ components/backend/Header.js
@@ -15,7 +15,7 @@ const Header = ({userData}) => {
                 <h1>Legal <strong>Corner</strong></h1>
               
                 <div className={styles.items}>
-                    <p>Welcome, <strong>{userData.name}</strong></p>
+                    {/* <p>Welcome, <strong>{userData.name}</strong></p> */}
                     {/* <img src="https://pbs.twimg.com/profile_images/1209556062270963712/qmftSQI9_400x400.jpg" alt="missing" /> */}
                     <Menu>
                         <MenuButton
